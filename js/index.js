a7d25ed5cbf5b43ec8cb9ff2e99fc007df19f7ce
Jake A. Niemiec <janiemiec20@gmail.com>
2019-02-14 17:40:49 +0000

    Updated colours for rainbow background and reformatted code

--- js/index.js
+++ js/index.js
@@ -1,57 +1,57 @@
 particlesJS("particles-js", {
-    particles: {
-        number: { value: 100, density: { enable: true, value_area: 900 } },
-        color: { value: "#ecf0f1" },
-        shape: {
-            type: "circle",
-            stroke: { width: 0, color: "#000000" },
-            polygon: { nb_sides: 5 },
-            // image: { src: "img/github.svg", width: 100, height: 100 }
-        },
-        opacity: {
-            value: 0.8,
-            random: false,
-            anim: { enable: false, speed: 10, opacity_min: 0.5, sync: false }
-        },
-        size: {
-            value: 3,
-            random: true,
-            anim: { enable: true, speed: 5, size_min: 0.5, sync: false }
-        },
-        line_linked: {
-            enable: true,
-            distance: 150,
-            color: "#919191",
-            opacity: 0.8,
-            width: 0.5
-        },
-        move: {
-            enable: true,
-            speed: 1.0,
-            direction: "none",
-            random: true,
-            straight: false,
-            out_mode: "bounce",
-            bounce: true,
-            attract: { enable: false, rotateX: 600, rotateY: 1200 }
-        }
+  particles: {
+    number: {value: 100, density: {enable: true, value_area: 900}},
+    color: {value: "#ecf0f1"},
+    shape: {
+      type: "circle",
+      stroke: {width: 0, color: "#ecf0f1"},
+      polygon: {nb_sides: 5},
+      // image: { src: "img/github.svg", width: 100, height: 100 }
     },
-    interactivity: {
-        detect_on: "canvas",
-        events: {
-            onhover: { enable: true, mode: "grab" },
-            onclick: { enable: true, mode: "push" },
-            resize: true
-        },
-        modes: {
-            grab: { distance: 300, line_linked: { opacity: 0.5 } },
-            bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-            repulse: { distance: 200, duration: 0.4 },
-            push: { particles_nb: 1 },
-            remove: { particles_nb: 2 }
-        }
+    opacity: {
+      value: 0.8,
+      random: false,
+      anim: {enable: false, speed: 10, opacity_min: 0.5, sync: false}
     },
-    retina_detect: true
+    size: {
+      value: 3,
+      random: true,
+      anim: {enable: true, speed: 5, size_min: 0.5, sync: false}
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ecf0f1",
+      opacity: 0.4,
+      width: 0.55
+    },
+    move: {
+      enable: true,
+      speed: 1.0,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "bounce",
+      bounce: true,
+      attract: {enable: false, rotateX: 600, rotateY: 1200}
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {enable: true, mode: "grab"},
+      onclick: {enable: true, mode: "push"},
+      resize: true
+    },
+    modes: {
+      grab: {distance: 300, line_linked: {opacity: 0.5}},
+      bubble: {distance: 400, size: 40, duration: 2, opacity: 8, speed: 3},
+      repulse: {distance: 200, duration: 0.4},
+      push: {particles_nb: 1},
+      remove: {particles_nb: 2}
+    }
+  },
+  retina_detect: true
 });
 // var count_particles, stats, update;
 // stats = new Stats();
@@ -61,12 +61,12 @@ particlesJS("particles-js", {
 // stats.domElement.style.top = "0px";
 // document.body.appendChild(stats.domElement);
 // count_particles = document.querySelector(".js-count-particles");
-update = function() {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
+update = function () {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
 };
 requestAnimationFrame(update);
