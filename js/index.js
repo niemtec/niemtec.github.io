64f4fa85dcf8678b51d8fa034100ac3b133d5eae
Jake <janiemiec20@gmail.com>
2019-08-15 01:30:45 +0100

    Added new infinity style bowtie. Reduced number of nodes. Changed colour scheme to dark

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 particlesJS("particles-js", {
   particles: {
-    number: {value: 100, density: {enable: true, value_area: 900}},
-    color: {value: "#616161"},
+    number: {value: 40, density: {enable: true, value_area: 900}},
+    color: {value: "#797979"},
     shape: {
       type: "circle",
-      stroke: {width: 0, color: "#616161"},
+      stroke: {width: 0, color: "#797979"},
       polygon: {nb_sides: 5},
       // image: { src: "img/github.svg", width: 100, height: 100 }
     },
@@ -14,14 +14,14 @@ particlesJS("particles-js", {
       anim: {enable: false, speed: 10, opacity_min: 0.2, sync: false}
     },
     size: {
-      value: 3,
+      value: 4,
       random: true,
       anim: {enable: true, speed: 5, size_min: 1.5, sync: false}
     },
     line_linked: {
       enable: true,
       distance: 150,
-      color: "#616161",
+      color: "#797979",
       opacity: 0.6,
       width: 0.70
     },
